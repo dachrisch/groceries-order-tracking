932477d83d5715a898f814b852a622089bfc7a69
dachrisch <christian.daehn@gmail.com>
2026-03-18 18:57:18 +0100

    feat: add Integration model; remove legacy knusprCredentials from User

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,11 +4,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  knusprCredentials: {
-    headers: { type: Map, of: String },
-    cookie: { type: String },
-    lastImport: { type: Date }
-  }
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
